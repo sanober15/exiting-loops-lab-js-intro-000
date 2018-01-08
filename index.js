548e39f10127f7360654d78f1a4262a7afc445d2
sanober riaz <sanober@sanober.me>
2018-01-08 06:22:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function keepGoing(array, changeValue, skipValue){
   return array;
 }
 
-function findBy(array, FindFn){
+function findBy(array, findFn){
   for (var i = 0; i < array.length; i++) {
     if(findFn(array[i])){
       return array[i];
